e2da6adcaa1b93af18d7fb21e0828a0c8bbc2913
Dmitriy V Kozubskiy <kozubskiyd@vezubr.ru>
2019-09-16 02:17:38 +0700

    config loader accepts default config file

--- bin/codexsoft.json-api.php
+++ bin/codexsoft.json-api.php
@@ -1,15 +1,59 @@
 <?php
 
-use CodexSoft\Code\Helpers\Cli;
 use CodexSoft\JsonApi\Operations\CreateActionOperation;
 use CodexSoft\OperationsSystem\Command\ExecuteOperationCommand;
 use Symfony\Component\Console\Command\Command;
 
 require_once __DIR__.'/findautoloader.php';
-$jsonApiConfigFile = Cli::getFirstArgumentOrDie();
+
+/**
+ * @return string
+ * @throws \Exception
+ */
+function unshiftFirstArgumentIfCorrectFilepath(): ?string
+{
+    if (PHP_SAPI !== 'cli') {
+        throw new \Exception('Configuration loading works only from CLI');
+    }
+
+    if (count($_SERVER['argv']) < 2) {
+        return null;
+    }
+
+    $configFile = realpath($_SERVER['argv'][1]);
+    if (substr($configFile, -strlen('.php')) !== '.php') {
+        return null;
+    }
+
+    if (!file_exists($configFile)) {
+        throw new \Exception("config file $configFile does not exists!\n");
+    }
+    unset($_SERVER['argv'][1]);
+
+    return $configFile;
+}
+
+/**
+ * @return string
+ * @throws Exception
+ */
+function getDefaultConfigFilepath(): string
+{
+    foreach ([
+                 __DIR__.'/../../../codexsoft.json-api.php',
+                 __DIR__.'/../config/codexsoft.json-api.php',
+             ] as $candidate) {
+        if (\file_exists($candidate)) {
+            return realpath($candidate);
+        }
+    }
+    throw new \Exception('Default config file not exists!');
+}
+
+$jsonApiConfigFile = unshiftFirstArgumentIfCorrectFilepath() ?: getDefaultConfigFilepath();
 $jsonApiSchema = \CodexSoft\JsonApi\JsonApiSchema::getFromConfigFile($jsonApiConfigFile);
 
-$console = new \Symfony\Component\Console\Application('CodexSoft JSON API tools CLI');
+$console = new \Symfony\Component\Console\Application("CodexSoft JSON API tools CLI\nConfig file: $jsonApiConfigFile");
 
 $jsonApi = new \CodexSoft\JsonApi\JsonApiTools;
 $jsonApi->setConfig($jsonApiSchema);
