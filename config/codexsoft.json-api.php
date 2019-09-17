803200fb90afa53ec1e79db58a49aa2ba27043d4
Dmitriy V Kozubskiy <kozubskiyd@vezubr.ru>
2019-09-18 00:49:18 +0700

    remove configs

--- config/codexsoft.json-api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return (new \CodexSoft\JsonApi\JsonApiSchema)
-    ->setNamespaceBase('TestApi')
-    ->setPathToPsrRoot(dirname(__DIR__).'/tests/unit');
-    //->setPathToPsrRoot(dirname(__DIR__).'/src');
